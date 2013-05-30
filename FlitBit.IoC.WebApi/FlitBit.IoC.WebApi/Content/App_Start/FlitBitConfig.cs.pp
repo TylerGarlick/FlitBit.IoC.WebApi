735efad418018d4b294fe59e36f1bed048837c20
Tyler Garlick <tjgarlick@gmail.com>
2013-05-29 23:54:47 -0400

    1.0.1 - Namespacing Issue

--- FlitBit.IoC.WebApi/FlitBit.IoC.WebApi/Content/App_Start/FlitBitConfig.cs.pp
+++ FlitBit.IoC.WebApi/FlitBit.IoC.WebApi/Content/App_Start/FlitBitConfig.cs.pp
@@ -1,9 +1,10 @@
 ﻿using System.Web.Http;
 using System.Web.Http.Dispatcher;
 using $rootnamespace$.App_Start;
+using FlitBit.IoC.WebApi;
 using WebActivatorEx;
 
-[assembly: PreApplicationStartMethod(typeof(FlitBitConfig), "PreStart")]
+[assembly: PreApplicationStartMethod(typeof($rootnamespace$.App_Start.FlitBitConfig), "PreStart")]
 namespace $rootnamespace$.App_Start
 {
     public static class FlitBitConfig
