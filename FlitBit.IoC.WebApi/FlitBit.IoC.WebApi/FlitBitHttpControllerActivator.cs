7d50245d3c935ef9dbd5139dda2fb748b4b58bf9
Tyler Garlick <tjgarlick@gmail.com>
2013-06-05 01:41:20 -0400

    Issues with CanConstruct()

--- FlitBit.IoC.WebApi/FlitBit.IoC.WebApi/FlitBitHttpControllerActivator.cs
+++ FlitBit.IoC.WebApi/FlitBit.IoC.WebApi/FlitBitHttpControllerActivator.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Net.Http;
+using System.Reflection;
 using System.Web.Http.Controllers;
 using System.Web.Http.Dispatcher;
 using FlitBit.Core;
+using FlitBit.Emit;
 
 namespace FlitBit.IoC.WebApi
 {
     public class FlitBitHttpControllerActivator : IHttpControllerActivator
     {
+        static readonly MethodInfo CreateNewMethod = typeof(FlitBitHttpControllerActivator).MatchGenericMethod("CreateNew", BindingFlags.Instance | BindingFlags.NonPublic, 1, typeof(object));
         public IHttpController Create(HttpRequestMessage request, HttpControllerDescriptor controllerDescriptor, Type controllerType)
         {
-            var currentFactory = FactoryProvider.Factory;
+            var create = CreateNewMethod.MakeGenericMethod(controllerType);
+            return create.Invoke(this, new object[] { }) as IHttpController;
+        }
 
-            if (currentFactory.CanConstruct(controllerType))
-                return currentFactory.CreateInstance(controllerType) as IHttpController;
-            
-            return default(IHttpController);
+        T CreateNew<T>()
+        {
+            var currentFactory = FactoryProvider.Factory;
+            return currentFactory.CanConstruct<T>() ?
+                currentFactory.CreateInstance<T>() :
+                default(T);
         }
     }
 }
