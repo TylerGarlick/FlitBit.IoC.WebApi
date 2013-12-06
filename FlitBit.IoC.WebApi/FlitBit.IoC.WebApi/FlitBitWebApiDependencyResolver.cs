06dd3413fff980d865faefd537dfdfc94d1a3a2d
Tyler Garlick <tjgarlick@gmail.com>
2013-12-06 14:04:31 -0500

    removed uncessary things from web api

--- FlitBit.IoC.WebApi/FlitBit.IoC.WebApi/FlitBitWebApiDependencyResolver.cs
+++ FlitBit.IoC.WebApi/FlitBit.IoC.WebApi/FlitBitWebApiDependencyResolver.cs
@@ -1,21 +1,74 @@
-﻿using System.Web.Http.Dependencies;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Dependencies;
+using FlitBit.Core;
 
 namespace FlitBit.IoC.WebApi
 {
-    public class FlitBitWebApiDependencyResolver : FlitBitWebApiContainerSharedScope, IDependencyResolver
+    public class FlitBitWebApiDependencyResolver : IDependencyResolver
     {
-        readonly IContainer _container;
+        IContainer _container;
 
-        public FlitBitWebApiDependencyResolver(IContainer container)
-            : base(container)
+        public FlitBitWebApiDependencyResolver()
         {
-            _container = container;
+            _container = Container.Current;
         }
 
         public virtual IDependencyScope BeginScope()
         {
             var childContainer = _container.MakeChildContainer(CreationContextOptions.InstanceTracking);
-            return new FlitBitWebApiContainerSharedScope(childContainer);
+            return new DependencyScope(childContainer);
+        }
+
+        public virtual object GetService(Type serviceType)
+        {
+            if (_container.CanConstruct(serviceType))
+                return _container.CreateInstance(serviceType);
+
+            return default(Type);
+        }
+
+        public virtual IEnumerable<object> GetServices(Type serviceType)
+        {
+            if (_container.CanConstruct(serviceType))
+            {
+                var enumerable = typeof(IEnumerable<>).MakeGenericType(serviceType);
+                return (IEnumerable<object>)_container.NewUntyped(LifespanTracking.Automatic, enumerable);
+            }
+
+            return Enumerable.Empty<object>();
+        }
+
+        public void Dispose()
+        {
+            Util.Dispose(ref _container);
+        }
+
+        class DependencyScope : IDependencyScope
+        {
+            IContainer _container;
+
+            public DependencyScope(IContainer container)
+            {
+                _container = container;
+            }
+
+            public void Dispose()
+            {
+                Util.Dispose(ref _container);
+            }
+
+            public object GetService(Type serviceType)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerable<object> GetServices(Type serviceType)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
+
 }
